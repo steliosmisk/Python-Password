26da1911f4544aa6357fb6dccba7e237dd3f2a32
Stelios Miskedakis <123480703+steliosmisk@users.noreply.github.com>
2023-01-24 17:38:05 +0200

    Add files via upload

---
+++ password.py
@@ -0,0 +1,21 @@
+import secrets
+import string
+
+print("--WELCOME TO MY PASSWORD GENERATOR--")
+print("[1] Start")
+print("[2] Exit")
+
+
+password_letters = string.ascii_letters + string.digits + string.punctuation
+password = ""
+
+user_choice = input("Enter your choice: ")
+while user_choice == "1" and user_choice != "2":
+    try:
+        user_pass_length = int(input("Enter your password length: "))
+        for _ in range(user_pass_length):
+            password += "".join(secrets.choice(password_letters))
+        print("Your Password is:", password)
+        break
+    except ValueError:
+        print("Error! Enter the length not a poem jk.")
