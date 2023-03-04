f4f24d661d3d507a84399fb10faba5d0edacfc19
Stelios Miskedakis <123480703+steliosmisk@users.noreply.github.com>
2023-03-04 08:43:36 +0200

    Update password.py

--- password.py
+++ password.py
@@ -1,21 +1,23 @@
 import secrets
 import string
 
-print("--WELCOME TO MY PASSWORD GENERATOR--")
-print("[1] Start")
+print("---- WELCOME TO MY PASSWORD GENERATOR ----")
+print("[1] Generate Password")
 print("[2] Exit")
 
-
 password_letters = string.ascii_letters + string.digits + string.punctuation
-password = ""
 
-user_choice = input("Enter your choice: ")
-while user_choice == "1":
-    try:
-        user_pass_length = int(input("Enter your password length: "))
-        for _ in range(user_pass_length):
-            password += "".join(secrets.choice(password_letters))
-        print("Your Password is:", password)
+while True:
+    user_choice = input("Enter your choice (1/2): ")
+    if user_choice == "1":
+        try:
+            user_pass_length = int(input("Enter the length of your password: "))
+            password = "".join(secrets.choice(password_letters) for _ in range(user_pass_length))
+            print("Your Password is:", password)
+        except ValueError:
+            print("Invalid input! Please enter a valid password length.")
+    elif user_choice == "2":
         break
-    except ValueError:
-        print("Error! Enter the length not a poem jk.")
+
+    else:
+        print("Invalid choice! Please enter a valid choice (1/2).")
